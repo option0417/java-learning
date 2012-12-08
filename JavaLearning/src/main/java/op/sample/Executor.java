953bf778472db1fc3da743d806b6ee60e17fc526
option0417 <option0417@gmail.com>
2012-12-09 00:51:24 +0800

    Make some refactor

--- JavaLearning/src/main/java/op/sample/Executor.java
+++ JavaLearning/src/main/java/op/sample/Executor.java
@@ -1,15 +1,11 @@
 package op.sample;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.net.URI;
+
+import op.sample.webservice.rs.JSONServiceClient;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.codehaus.jettison.json.JSONObject;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 
 public class Executor {
@@ -17,35 +13,10 @@ public class Executor {
 	public static void main(String[] args) throws Exception {
 //		new Prepopulate().prepopulate();
 //		LOG.debug("** Database filled. **");
-		try {
-			Client client = Client.create();
-	 
-			WebResource webResource = client
-			   .resource("http://127.0.0.1:8080/JavaLearning/rest/json/metallica/post");
-			
-			JSONObject jsonObj = new JSONObject();			
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			jsonObj.put("recordDate", sdf.format(new Date(System.currentTimeMillis())).toString());
-			jsonObj.put("itemName", "Fade To Black");
-			jsonObj.put("price", "2468");
-	 
-			ClientResponse response = webResource.type("application/json")
-			   .post(ClientResponse.class, jsonObj.toString());
-	 
-			if (response.getStatus() != 201) {
-				throw new RuntimeException("Failed : HTTP error code : "
-				     + response.getStatus());
-			}
-	 
-			System.out.println("Output from Server .... \n");
-			String output = response.getEntity(String.class);
-			System.out.println(output);
-	 
-		  } catch (Exception e) {
-	 
-			e.printStackTrace();
-	 
-		  }		
+		
+		JSONServiceClient client = new JSONServiceClient();
+		client.setURI(new URI(""));
+		client.sendRequest();
 	}
 }
 
--- JavaLearning/src/main/java/op/sample/log4j/TestLog4j.java
+++ JavaLearning/src/main/java/op/sample/log4j/TestLog4j.java
@@ -3,9 +3,6 @@ package op.sample.log4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
-
 public class TestLog4j {
 	private static final  Logger logger = LogManager.getLogger(TestLog4j.class.getName());
 	
