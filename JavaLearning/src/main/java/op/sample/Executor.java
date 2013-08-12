84b8189ea21f954ea5129c5a32e32d585ee5c607
option0417 <option0417@gmail.com>
2013-08-12 22:11:21 +0800

    Add NIO Sample Add RandomGenerator

--- JavaLearning/src/main/java/op/sample/Executor.java
+++ JavaLearning/src/main/java/op/sample/Executor.java
@@ -1,61 +1,112 @@
 package op.sample;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-import op.sample.spring.exception.TestException;
+import op.sample.random.RandomGenerator;
+import op.sample.random.RandomThread;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
 
 
-public class Executor {
+public class Executor extends DefaultHandler {
 	private static final Logger LOG = LogManager.getLogger(Executor.class);
-	//private static final ClassPathXmlApplicationContext appContext =
-	//		new ClassPathXmlApplicationContext("opSample-beans-config.xml");
+	private static final List<String> xsdURIList = new ArrayList<String>();
+	public Executor() {
+		super();
+	}
+	
 	public static void main(String[] args) throws Exception {
-//		new Prepopulate().prepopulate();
-//		LOG.debug("** Database filled. **");
-		
-//		JSONServiceClient client = new JSONServiceClient();
-//		client.setURI(new URI(""));
-//		client.sendRequest();
-		
-//		try {
-//			appContext.getBean(TestException.class).testException();
-//			System.out.println("Done");
-//		} catch (Exception e) {
-//			System.out.println("Exception occured.");
+//		URL url = new URL("http://localhost:8080/mep-innser-service-stub/MEPInnerService?wsdl");
+//		URLConnection urlConnection = url.openConnection();
+//		
+//		InputStream is = urlConnection.getInputStream();
+//		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+//		int data = 0;
+//		
+//		while ((data = is.read()) != -1) {
+//			buffer.write(data);
+//		}
+//		is.close();
+//		ByteArrayInputStream bais = new ByteArrayInputStream(buffer.toByteArray());
+//		LOG.info(buffer.toString());
+//		buffer.close();
+//		
+//		Executor e = new Executor();
+//		XMLReader xr = XMLReaderFactory.createXMLReader();
+//		xr.setContentHandler(e);
+//		xr.setErrorHandler(e);
+//		xr.parse(new InputSource(bais));
+//		
+//		for (String s : e.getXSDURIList()) {
+//			LOG.info(getURLContent(new URL(s)));
 //		}
 		
-		File directory = new File("/home/option0417/Dev/EclipsePlatform/JEEWorkspace/mep");
-		delDir(directory, "_svn");
+		final int range = 10;
+		RandomGenerator randomGenerator = new RandomGenerator();
+		randomGenerator.setNumberRange(range);
 		
+		RandomThread t1 = new RandomThread(1, randomGenerator);
+		RandomThread t2 = new RandomThread(2, randomGenerator);
+		RandomThread t3 = new RandomThread(3, randomGenerator);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
+//		Random rand = new Random();
+//		for (int i = 0; i < 100; i++) {
+//			System.out.println(rand.nextInt(10));
+//		}
 	}
 	
-	private static void delDir(File targetDir, String dirName) {
-		for (File tmpFile : targetDir.listFiles()) {
-			if (tmpFile.isDirectory()) {
-				if (tmpFile.getName().equals(dirName)) {
-					System.out.println(tmpFile.getName());
-					delDir(tmpFile);
-				} else {
-					delDir(tmpFile, dirName);
-				}
-			}
+	public static String getURLContent(URL url) throws IOException {
+		URLConnection urlConnection = url.openConnection();
+		
+		InputStream is = urlConnection.getInputStream();
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		int data = 0;
+		
+		while ((data = is.read()) != -1) {
+			buffer.write(data);
 		}
+		is.close();
+		return buffer.toString("UTF8");
 	}
 	
-	private static void delDir(File targetDir) {
-		for (File tmpFile : targetDir.listFiles()) {
-			if (tmpFile.isDirectory()) {
-				delDir(tmpFile);
-			} else {
-				System.out.println(tmpFile.getName() + " will deleted");
-				tmpFile.delete();
-			}
-		}
-		targetDir.delete();
+    public void startDocument () {
+    	System.out.println("Start document");
+    }
+
+    public void endDocument () {
+    	System.out.println("End document");
+    }
+    
+    public void startElement (String uri, String name, String qName, Attributes atts) {
+    	if ("import".equals(name)) {
+    		handleAtts(atts);
+    	}
 	}
+    
+    private void handleAtts(Attributes atts) {
+    	for (int index = 0; index < atts.getLength(); index++) {
+    		if ("schemaLocation".equals(atts.getLocalName(index))) {
+    			xsdURIList.add(atts.getValue(index));
+    		}
+    	}
+    }
+    
+    public List<String> getXSDURIList() {
+    	return xsdURIList;
+    }
 }
 
